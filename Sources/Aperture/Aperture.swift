81722aebd3b1a7bc46a7c15868bf35e8db26551b
Ned Rockson <nedrocks@gmail.com>
2021-01-05 19:47:39 -0800

    Adding default to medium capture quality.

--- Sources/Aperture/Aperture.swift
+++ Sources/Aperture/Aperture.swift
@@ -31,6 +31,7 @@ public final class Aperture: NSObject {
 	) throws {
 		self.destination = destination
 		self.session = AVCaptureSession()
+		self.session.Preset = AVCaptureSessionPresetMedium
 		self.output = output
 
 		// Needed because otherwise there is no audio on videos longer than 10 seconds.
