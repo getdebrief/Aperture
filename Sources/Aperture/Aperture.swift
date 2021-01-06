916fb0063f464378eaeac6e49bd8223b64494db3
Ned Rockson <nedrocks@gmail.com>
2021-01-05 22:36:21 -0800

    I can spell

--- Sources/Aperture/Aperture.swift
+++ Sources/Aperture/Aperture.swift
@@ -31,7 +31,7 @@ public final class Aperture: NSObject {
 	) throws {
 		self.destination = destination
 		self.session = AVCaptureSession()
-		self.session.SessionPreset = AVCaptureSessionPresetMedium
+		self.session.sessionPreset = AVCaptureSessionPresetMedium
 		self.output = output
 
 		// Needed because otherwise there is no audio on videos longer than 10 seconds.
